81282a8152798197c013d32af56fee11631e102d
NEERAJ KUMAR <49531334+Kumarbegnier@users.noreply.github.com>
2023-04-25 10:16:03 +0530

    Create code.py

---
+++ 30 Day Challenge/Hello Techgig/code.py
@@ -0,0 +1,16 @@
+# Read input from STDIN. Print your output to STDOUT
+# Use input() to read input from STDIN and use print to write your output to STDOUT
+
+def main():
+    # Read input from STDIN
+    input_str = input()
+
+    # Write your code logic here
+    output_str = "Hello Techgig\n" + input_str
+
+    # Print your output to STDOUT
+    print(output_str)
+
+# Call the main function to run the code
+if __name__ == '__main__':
+    main()
